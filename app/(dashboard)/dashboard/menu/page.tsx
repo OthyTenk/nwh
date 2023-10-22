722e3f180222496a94e6ae9d884d5860c1fdf13e
Othy Tenk <othy.tenk@gmail.com>
2023-10-22 16:00:25 +0800

    feat: adds menu page for dashboard

---
+++ app/(dashboard)/dashboard/menu/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminMenuTable from "./AdminMenuTable";
+import Container from "@/app/components/common/Container";
+
+const AdminMenus = () => {
+  return (
+    <Container>
+      <AdminMenuTable />
+    </Container>
+  );
+};
+
+export default AdminMenus;
