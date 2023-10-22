91d7a25f2c12859008acabe230cede1bd093da71
Othy Tenk <othy.tenk@gmail.com>
2023-10-22 14:03:03 +0800

    feat: adds the order page

---
+++ app/(user)/user/orders/page.tsx
@@ -0,0 +1,10 @@
+import Container from "@/app/components/common/Container";
+import UserOrders from "./UserOrders";
+
+export default function UserOrderPage() {
+  return (
+    <Container>
+      <UserOrders />
+    </Container>
+  );
+}
